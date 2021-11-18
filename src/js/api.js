9c382fdd36d8aa83d5581640029b31a84c8b8956
Сергей Смирнов <sergio.feriny@gmail.com>
2021-11-19 00:28:16 +0200

    start

---
+++ src/js/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api';
+const KEY = '24409833-1a93e7bc2e210ce3f7f0d3f0b';
+
+export async function fetchImages(query, page, pageSize) {
+  const response = await axios.get(
+    `${BASE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${pageSize}`,
+    );
+    console.log(response);
+  return response;
+}
+
+export default {fetchImages};
+
